dd15fbe9b9a322d04ba80de6722a72bdc5b3e156
joepool <65784403+joepool@users.noreply.github.com>
2021-02-04 18:33:01 +0000

    KeyPress

--- src/ptt/KeyPress.java
+++ src/ptt/KeyPress.java
@@ -1,9 +1,28 @@
 package ptt;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class KeyPress {
 
 	public static void main(String[] args){
+			try {
+				String s = null;
+				String[] cmd = {
+						"/bin/bash",
+						"-c",
+						"./script_python"
+						};
 
+						Process p = Runtime.getRuntime().exec(cmd);
+				BufferedReader in = new BufferedReader(new InputStreamReader(p.getInputStream()));
+				while ((s = in.readLine()) != null) {
+					System.out.println(s);
+				}
+				}
+			catch(IOException ie) {
+				ie.printStackTrace();
+			}
 	}
 
 }
